ce6c3973a02ba2ab6a768b88df1d23e4db85f397
mariofusco <mario.fusco@gmail.com>
2022-11-24 17:50:50 +0100

    minor changes in CountingOutput

--- src/main/java/gameoflife/ui/CountingOutput.java
+++ src/main/java/gameoflife/ui/CountingOutput.java
@@ -3,20 +3,23 @@ package gameoflife.ui;
 import java.util.function.Consumer;
 
 public class CountingOutput implements Consumer<boolean[][]> {
-    static final int TIMED_frames = 1000;
-    static final int WARMUP_frames = TIMED_frames;
+    static final int TIMED_frames = 5000;
+    static final int WARMUP_frames = 2000;
 
     private int frames = 0;
     private long timingStartNanos;
 
     @Override
     public void accept(boolean[][] cells) {
+        if (frames == 0) {
+            System.out.println("\nStarting warmup\n");
+        }
         frames++;
         if ((frames & 0xf) == 0) {
             System.out.print(".");
         }
         if (frames == WARMUP_frames) {
-            System.out.printf("%nStarting timer");
+            System.out.println("\n\nStarting benchmark\n");
             timingStartNanos = System.nanoTime();
         } else if (frames == WARMUP_frames + TIMED_frames) {
             long millis = (System.nanoTime() - timingStartNanos) / 1_000_000;
